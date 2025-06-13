d95d79d0b060d1300c9a59becc43c7c629a4e9c4
Yu Chun,Chou <a830144@gmail.com>
2025-06-13 03:12:38 -0500

    change for openAPI

--- src/main/java/com/mysap/sd/product/controller/ProductController.java
+++ src/main/java/com/mysap/sd/product/controller/ProductController.java
@@ -1,10 +1,17 @@
 package com.mysap.sd.product.controller;
 
-import com.mysap.sd.product.entity.Product;
-import com.mysap.sd.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mysap.sd.product.entity.Product;
+import com.mysap.sd.product.repository.ProductRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/products")
@@ -14,6 +21,7 @@ public class ProductController {
     private ProductRepository repository;
 
     @PostMapping
+    @Operation(summary = "ADD A NEW PRODUCT")
     public ResponseEntity<?> createProduct(@RequestBody Product product) {
         if (product.getCode() == null || product.getName() == null) {
             return ResponseEntity.badRequest().body("Code and Name are required.");
@@ -26,6 +34,7 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(summary = "Get ALL products")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(repository.findAll());
     }
